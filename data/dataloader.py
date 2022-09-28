15d24257dc0f83107005169408552b4782da2bb3
caisarl76 <caisarl76@gmail.com>
2022-09-28 15:32:42 +0900

    check imgnet bug

--- data/dataloader.py
+++ data/dataloader.py
@@ -135,7 +135,7 @@ def load_data(data_root, dataset, phase, batch_size, sampler_dic=None, num_worke
     elif dataset == 'CIFAR100_LT':
         print('====> CIFAR100 Imbalance Ratio: ', cifar_imb_ratio)
         set_ = IMBALANCECIFAR100(phase, imbalance_ratio=cifar_imb_ratio, root=data_root)
-    elif dataset in ['iNaturalist18', 'imagenet']:
+    elif dataset in ['iNaturalist18', 'imagenet', 'places']:
         rgb_mean, rgb_std = RGB_statistics[key]['mean'], RGB_statistics[key]['std']
         if phase not in ['train', 'val']:
             transform = get_data_transform('test', rgb_mean, rgb_std, key)
