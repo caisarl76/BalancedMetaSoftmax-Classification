1472a0c0d3153f42eedcb11327a36a9d1508d977
caisarl76 <caisarl76@gmail.com>
2022-10-24 15:38:10 +0900

    add balms json

--- binary_train.py
+++ binary_train.py
@@ -120,8 +120,9 @@ def update(config, args):
             config['training_opt']['num_epochs'] = 10
             config['training_opt'].pop('num_iterations')
         else:
-            config['training_opt']['num_epochs'] = 20
-            config['training_opt'].pop('num_iterations')
+            if config['training_opt']['sampler'] is None:
+                config['training_opt']['num_epochs'] = 20
+                config['training_opt'].pop('num_iterations')
 
     if 'freq_path' in config['criterions']['PerformanceLoss']['loss_params']:
         config['criterions']['PerformanceLoss']['loss_params']['freq_path'] = os.path.join('cls_freq',
